440d377d63a9bac423029ad9d7f5672f3aa4ce38
nicat ceferli <mega.ceferli@gmail.com>
2021-10-05 16:03:48 +0400

    dd

---
+++ Dependency Inversion/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Inversion
+{
+
+    interface IDataFormatter
+    {
+        void Convert();
+    }
+    class JsonFormatter : IDataFormatter
+    {
+        public void Convert()
+        {
+            Console.WriteLine("Json Formatina Cevrildi");
+        }
+    }
+    class XMLFormatter : IDataFormatter
+    {
+        public void Convert()
+        {
+            Console.WriteLine("XML Formatina Cevrildi");
+        }
+    }
+
+
+
+    class FormatConverter
+    {
+        public IDataFormatter Format { get; set; }
+        public FormatConverter(IDataFormatter Format)
+        {
+            this.Format = Format;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FormatConverter jsonConvert= new FormatConverter(new JsonFormatter());
+            jsonConvert.Format.Convert();
+
+
+            FormatConverter XmlConvert = new FormatConverter(new XMLFormatter());
+            XmlConvert.Format.Convert();
+
+        }
+
+
+    }
+}
